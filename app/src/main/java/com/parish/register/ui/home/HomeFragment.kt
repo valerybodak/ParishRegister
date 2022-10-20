9c9541b12d7affaa2b2be5e5429b29a4b49b22a6
Valeriy Bodak <valery.bodak@exposit.com>
2022-10-20 19:20:33 +0300

    updated home fragment

--- app/src/main/java/com/parish/register/ui/home/HomeFragment.kt
+++ app/src/main/java/com/parish/register/ui/home/HomeFragment.kt
@@ -6,37 +6,43 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.parish.register.databinding.FragmentHomeBinding
+import kotlinx.coroutines.Job
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    //private val args by navArgs<QuizFragmentArgs>()
+    private val viewModel by viewModels<HomeViewModel>()
+    private var binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //viewModel.initArgs(Category.getById(args.categoryId))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    ): View? {
+        if (binding == null) {
+            binding = FragmentHomeBinding.inflate(inflater, container, false)
         }
-        return root
+        return binding?.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //setupToolbar(binding?.toolbarDefault?.toolbar, false, getString(R.string.books))
+        //hideNavigationIcon(binding?.toolbarDefault?.toolbar)
+        // if (isNeedToInitViews()) {
+        //initViews()
+        //}
+        //initFields()
+        //initSubscribers()
+        //viewModel.getQuiz()
     }
 }
