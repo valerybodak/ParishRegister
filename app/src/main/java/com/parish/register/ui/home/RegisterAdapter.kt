b147beed70edc0becf27ea0846a45c5f70ef1059
Valeriy Bodak <valery.bodak@exposit.com>
2022-10-24 10:06:15 +0300

    added Died to adapter

--- app/src/main/java/com/parish/register/ui/home/RegisterAdapter.kt
+++ app/src/main/java/com/parish/register/ui/home/RegisterAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.parish.register.databinding.ItemRegisterBinding
 import com.parish.register.model.Born
+import com.parish.register.model.Died
 import com.parish.register.model.ListItem
 import com.parish.register.model.Marriage
 
@@ -20,17 +21,28 @@ class RegisterAdapter(
 
     override fun getItemViewType(position: Int): Int {
         val item = data[position]
-        if (item is Born) {
-            return VIEW_TYPE_BORN
+        return if (item is Born) {
+            VIEW_TYPE_BORN
+        } else if (item is Marriage) {
+            VIEW_TYPE_MARRIAGE
         } else {
-            return VIEW_TYPE_MARRIAGE
+            VIEW_TYPE_DIED
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
             VIEW_TYPE_BORN -> {
-                return BornItemHolder(
+                BornItemHolder(
+                    ItemRegisterBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+            VIEW_TYPE_MARRIAGE -> {
+                MarriageItemHolder(
                     ItemRegisterBinding.inflate(
                         LayoutInflater.from(parent.context),
                         parent,
@@ -39,7 +51,7 @@ class RegisterAdapter(
                 )
             }
             else -> {
-                return MarriageItemHolder(
+                DiedItemHolder(
                     ItemRegisterBinding.inflate(
                         LayoutInflater.from(parent.context),
                         parent,
@@ -64,6 +76,10 @@ class RegisterAdapter(
             VIEW_TYPE_MARRIAGE -> {
                 val viewHolder = holder as MarriageItemHolder
                 viewHolder.bind(item as Marriage)
+            }
+            VIEW_TYPE_DIED -> {
+                val viewHolder = holder as DiedItemHolder
+                viewHolder.bind(item as Died)
             }
         }
     }
@@ -148,8 +164,46 @@ class RegisterAdapter(
         }*/
     }
 
+    inner class DiedItemHolder(private val binding: ItemRegisterBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Died) {
+
+            binding.tvName.text = item.fullName
+
+            /*setBackgroundColor(item)
+
+            binding.title.text = item.titleRu
+            binding.author.text = item.authorRu
+
+            var imageUrl = DEFAULT_BOOK_IMAGE_URL
+            if (!item.imageUrl.isNullOrEmpty()) {
+                imageUrl = item.imageUrl ?: ""
+            }
+            Glide.with(itemView.context)
+                .load(imageUrl)
+                .transform(CenterCrop())
+                .into(binding.cover)*/
+
+            itemView.setOnClickListener {
+                listener?.onItemClick(item)
+            }
+        }
+
+        /*private fun setBackgroundColor(item: Book) {
+            val colorId = if(item.backgroundColorId == 0) R.color.book_bg_color_1 else item.backgroundColorId
+            binding.coverBackground.backgroundTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(
+                    itemView.context,
+                    colorId
+                )
+            )
+        }*/
+    }
+
     companion object {
         private const val VIEW_TYPE_BORN = 0
         private const val VIEW_TYPE_MARRIAGE = 1
+        private const val VIEW_TYPE_DIED = 2
     }
 }
