c8b3db06b4930796b14c337ae8e3b69061f17de2
Valeriy Bodak <valery.bodak@exposit.com>
2022-10-26 18:39:37 +0300

    updated interval

--- app/src/main/java/com/parish/register/common/SyncHelper.kt
+++ app/src/main/java/com/parish/register/common/SyncHelper.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 object SyncHelper {
 
-    private const val SYNC_INTERVAL_MILLIS = 3600 //1 hour
+    private const val SYNC_INTERVAL_MILLIS = 3600000 //1 hour
 
     fun isSyncNeed(lastSync: Long): Boolean {
         return (Calendar.getInstance().timeInMillis - lastSync) > SYNC_INTERVAL_MILLIS
