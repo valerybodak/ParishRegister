9bd8c8ad1106c2661530abc5cb3d0fd0af3b437b
Valeriy Bodak <valery.bodak@exposit.com>
2022-12-16 12:35:08 +0300

    code refactoring

--- app/src/main/java/com/parish/register/ui/custom/dashboard/DashboardView.kt
+++ app/src/main/java/com/parish/register/ui/custom/dashboard/DashboardView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
 
 class DashboardView @JvmOverloads constructor(
     context: Context,
@@ -17,39 +18,40 @@ class DashboardView @JvmOverloads constructor(
         super.onDraw(canvas)
 
         if (canvas != null && items != null) {
-            val height = canvas.height / 2
-            val center = Point(canvas.width / 2, canvas!!.height / 2)
-            val inner_radius = 130F
-            val outer_radius = height.toFloat()
-            val arc_sweep = 90F
-            val arc_ofset = 30F
+            val center = Point(width / 2, height / 2)
+            items?.forEach { item ->
+                val inner_radius = 130F
+                val outer_radius = height.toFloat() / 2
+                val arc_sweep = 90F
+                val arc_ofset = 30F
 
-            val outer_rect = RectF(
-                center.x - outer_radius,
-                center.y - outer_radius,
-                center.x + outer_radius,
-                center.y + outer_radius
-            )
-            val inner_rect = RectF(
-                center.x - inner_radius,
-                center.y - inner_radius,
-                center.x + inner_radius,
-                center.y + inner_radius
-            )
+                val outer_rect = RectF(
+                    center.x - outer_radius,
+                    center.y - outer_radius,
+                    center.x + outer_radius,
+                    center.y + outer_radius
+                )
+                val inner_rect = RectF(
+                    center.x - inner_radius,
+                    center.y - inner_radius,
+                    center.x + inner_radius,
+                    center.y + inner_radius
+                )
 
-            val path = Path()
-            path.arcTo(outer_rect, arc_ofset, arc_sweep)
-            path.arcTo(inner_rect, arc_ofset + arc_sweep, -arc_sweep)
-            path.close()
+                val path = Path()
+                path.arcTo(outer_rect, arc_ofset, arc_sweep)
+                path.arcTo(inner_rect, arc_ofset + arc_sweep, -arc_sweep)
+                path.close()
 
-            val fill = Paint()
-            fill.setColor(Color.GREEN)
-            canvas?.drawPath(path, fill)
+                val fill = Paint()
+                fill.setColor(ContextCompat.getColor(context, item.colorId))
+                canvas?.drawPath(path, fill)
 
-            /*val border = Paint()
+                /*val border = Paint()
                 border.setStyle(Paint.Style.STROKE)
                 border.setStrokeWidth(2F)
                 canvas?.drawPath(path, border)*/
+            }
         }
     }
 
