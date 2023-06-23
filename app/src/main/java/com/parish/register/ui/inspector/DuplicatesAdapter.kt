ac16ee627d53e2a63e176e11e4f3c99b0a476853
ValeryBodak <valery.bodak@telus.com>
2023-06-23 09:43:16 +0300

    added DuplicatesAdapter

---
+++ app/src/main/java/com/parish/register/ui/inspector/DuplicatesAdapter.kt
@@ -0,0 +1,74 @@
+package com.parish.register.ui.inspector
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.parish.register.common.CommonConsts.BACKEND_DATE_FORMAT
+import com.parish.register.databinding.ItemDuplicateBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DuplicatesAdapter(
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val dateFormat = SimpleDateFormat(BACKEND_DATE_FORMAT, Locale.ENGLISH)
+    private val items: MutableList<DuplicateItem> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return DiedItemHolder(
+            ItemDuplicateBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val viewHolder = holder as DiedItemHolder
+        viewHolder.bind(items[position])
+    }
+
+    fun update(newItems: List<DuplicateItem>) {
+        items.clear()
+        items.addAll(newItems)
+        notifyDataSetChanged()
+    }
+
+    inner class DiedItemHolder(private val binding: ItemDuplicateBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: DuplicateItem) {
+            binding.tvName.text = item.item1.getSortName()
+            /*binding.tvName.setHighlightedText(item.fullName, searchString)
+            bindDate(binding.tvDate, R.string.died, item.deathDate)
+            if (item.parents.isEmpty()) {
+                binding.tvParents.goneView()
+            } else {
+                binding.tvParents.showView()
+                binding.tvParents.setHighlightedText(
+                    itemView.context.getString(
+                        R.string.parents,
+                        item.parents
+                    ), searchString
+                )
+            }
+            itemView.setOnClickListener {
+                listener?.onItemClick(item)
+            }*/
+        }
+    }
+
+    companion object {
+        private const val VIEW_TYPE_BORN = 0
+        private const val VIEW_TYPE_MARRIAGE = 1
+        private const val VIEW_TYPE_DIED = 2
+
+        private const val UNKNOWN_VALUE = "???"
+        private const val DATE_FORMAT_TO_DISPLAY = "dd.MM.yyyy"
+    }
+}
--- app/src/main/java/com/parish/register/ui/inspector/InspectorFragment.kt
+++ app/src/main/java/com/parish/register/ui/inspector/InspectorFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.parish.register.R
 import com.parish.register.databinding.FragmentInspectorBinding
 import com.parish.register.ui.base.BaseFragment
+import com.parish.register.ui.custom.CommonItemDecoration
 
 class InspectorFragment : BaseFragment() {
 
     private val viewModel by viewModels<InspectorViewModel>()
     private var binding: FragmentInspectorBinding? = null
+
+    private var duplicatesAdapter: DuplicatesAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +38,28 @@ class InspectorFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews()
         initSubscribers()
+    }
+
+    private fun initViews() {
+        if (duplicatesAdapter == null) {
+            duplicatesAdapter = DuplicatesAdapter()
+            binding?.rvDuplicates?.apply {
+                layoutManager = LinearLayoutManager(context)
+                val dividerDecoration =
+                    DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+                dividerDecoration.setDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.list_divider
+                    )!!
+                )
+                addItemDecoration(dividerDecoration)
+                addItemDecoration(CommonItemDecoration(requireContext(), false))
+                adapter = duplicatesAdapter
+            }
+        }
     }
 
     private fun initSubscribers() {
@@ -42,6 +69,6 @@ class InspectorFragment : BaseFragment() {
     }
 
     private fun bindDuplicates(duplicates: List<DuplicateItem>) {
-
+        duplicatesAdapter?.update(duplicates)
     }
 }
