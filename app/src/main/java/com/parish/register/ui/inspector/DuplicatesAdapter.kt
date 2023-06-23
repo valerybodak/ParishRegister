64cca22fdca2a118d12239b7689e89d9516b1348
ValeryBodak <valery.bodak@telus.com>
2023-06-23 10:23:54 +0300

    diaplyed duplicates

--- app/src/main/java/com/parish/register/ui/inspector/DuplicatesAdapter.kt
+++ app/src/main/java/com/parish/register/ui/inspector/DuplicatesAdapter.kt
@@ -3,15 +3,18 @@ package com.parish.register.ui.inspector
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.parish.register.R
 import com.parish.register.common.CommonConsts.BACKEND_DATE_FORMAT
 import com.parish.register.databinding.ItemDuplicateBinding
+import com.parish.register.model.Born
+import com.parish.register.model.Died
+import com.parish.register.model.Marriage
 import java.text.SimpleDateFormat
 import java.util.*
 
 class DuplicatesAdapter(
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    private val dateFormat = SimpleDateFormat(BACKEND_DATE_FORMAT, Locale.ENGLISH)
     private val items: MutableList<DuplicateItem> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -42,33 +45,53 @@ class DuplicatesAdapter(
     inner class DiedItemHolder(private val binding: ItemDuplicateBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: DuplicateItem) {
-            binding.tvFund.text = item.item1.getSortName()
-            /*binding.tvName.setHighlightedText(item.fullName, searchString)
-            bindDate(binding.tvDate, R.string.died, item.deathDate)
-            if (item.parents.isEmpty()) {
-                binding.tvParents.goneView()
-            } else {
-                binding.tvParents.showView()
-                binding.tvParents.setHighlightedText(
-                    itemView.context.getString(
-                        R.string.parents,
-                        item.parents
-                    ), searchString
-                )
+        fun bind(duplicate: DuplicateItem) {
+            if (duplicate.item1 is Born) {
+                bindBornDuplicates(duplicate.item1, duplicate.item2 as Born)
+            } else if (duplicate.item1 is Marriage) {
+                bindMarriageDuplicates(duplicate.item1, duplicate.item2 as Marriage)
+            } else if (duplicate.item1 is Died) {
+                bindDiedDuplicates(duplicate.item1, duplicate.item2 as Died)
             }
-            itemView.setOnClickListener {
-                listener?.onItemClick(item)
-            }*/
         }
-    }
 
-    companion object {
-        private const val VIEW_TYPE_BORN = 0
-        private const val VIEW_TYPE_MARRIAGE = 1
-        private const val VIEW_TYPE_DIED = 2
+        private fun bindBornDuplicates(item1: Born, item2: Born) {
+            binding.ivIcon.setImageResource(R.drawable.ic_born)
+            binding.tvFund.text = itemView.context.getString(
+                R.string.fund_format,
+                item1.fundNumber,
+                item1.inventoryNumber,
+                item1.caseNumber,
+                item1.page
+            )
+            binding.tvDate.text = item1.birthDate
+            binding.tvContent.text = item1.fullName + "\n" + item2.fullName
+        }
 
-        private const val UNKNOWN_VALUE = "???"
-        private const val DATE_FORMAT_TO_DISPLAY = "dd.MM.yyyy"
+        private fun bindMarriageDuplicates(item1: Marriage, item2: Marriage) {
+            binding.ivIcon.setImageResource(R.drawable.ic_marriage)
+            binding.tvFund.text = itemView.context.getString(
+                R.string.fund_format,
+                item1.fundNumber,
+                item1.inventoryNumber,
+                item1.caseNumber,
+                item1.page
+            )
+            binding.tvDate.text = item1.date
+            binding.tvContent.text = item1.groom + "\n" + item2.groom
+        }
+
+        private fun bindDiedDuplicates(item1: Died, item2: Died) {
+            binding.ivIcon.setImageResource(R.drawable.ic_died)
+            binding.tvFund.text = itemView.context.getString(
+                R.string.fund_format,
+                item1.fundNumber,
+                item1.inventoryNumber,
+                item1.caseNumber,
+                item1.page
+            )
+            binding.tvDate.text = item1.deathDate
+            binding.tvContent.text = item1.fullName + "\n" + item2.fullName
+        }
     }
 }
--- app/src/main/java/com/parish/register/ui/inspector/InspectorViewModel.kt
+++ app/src/main/java/com/parish/register/ui/inspector/InspectorViewModel.kt
@@ -19,6 +19,8 @@ class InspectorViewModel @Inject constructor(
         viewModelScope.launch {
             val duplicates = mutableListOf<DuplicateItem>()
             duplicates.addAll(getBornDuplicates())
+            duplicates.addAll(getMarriageDuplicates())
+            duplicates.addAll(getDiedDuplicates())
             duplicatesLiveData.value = duplicates
         }
     }
@@ -33,8 +35,61 @@ class InspectorViewModel @Inject constructor(
                     if (item1.fundNumber == item2.fundNumber
                         && item1.inventoryNumber == item2.inventoryNumber
                         && item1.caseNumber == item2.caseNumber
+                        && item1.page == item2.page
                         && item1.birthDate == item2.birthDate
                         && item1.gender == item2.gender
+                    ) {
+                        duplicates.add(
+                            DuplicateItem(
+                                item1 = item1,
+                                item2 = item2
+                            )
+                        )
+                    }
+                }
+            }
+        }
+        return duplicates
+    }
+
+    private suspend fun getMarriageDuplicates(): List<DuplicateItem> {
+        val duplicates = mutableListOf<DuplicateItem>()
+        val marriage1 = registerRepository.getMarriageList()
+        val marriage2 = marriage1.toList()
+        marriage1.forEachIndexed { index1, item1 ->
+            marriage2.forEachIndexed { index2, item2 ->
+                if (index1 != index2) {
+                    if (item1.fundNumber == item2.fundNumber
+                        && item1.inventoryNumber == item2.inventoryNumber
+                        && item1.caseNumber == item2.caseNumber
+                        && item1.page == item2.page
+                        && item1.date == item2.date
+                    ) {
+                        duplicates.add(
+                            DuplicateItem(
+                                item1 = item1,
+                                item2 = item2
+                            )
+                        )
+                    }
+                }
+            }
+        }
+        return duplicates
+    }
+
+    private suspend fun getDiedDuplicates(): List<DuplicateItem> {
+        val duplicates = mutableListOf<DuplicateItem>()
+        val died1 = registerRepository.getDiedList()
+        val died2 = died1.toList()
+        died1.forEachIndexed { index1, item1 ->
+            died2.forEachIndexed { index2, item2 ->
+                if (index1 != index2) {
+                    if (item1.fundNumber == item2.fundNumber
+                        && item1.inventoryNumber == item2.inventoryNumber
+                        && item1.caseNumber == item2.caseNumber
+                        && item1.page == item2.page
+                        && item1.deathDate == item2.deathDate
                     ) {
                         duplicates.add(
                             DuplicateItem(
